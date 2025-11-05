69bb215f45f82127ed1e6b5096b643be5e865ea4
Daniel Palacios <2022371017@uteq.edu.mx>
2025-11-05 17:49:12 -0600

    Microservicio de materias

--- src/main/java/com/materias/materias/controllers/MateriasController.java
+++ src/main/java/com/materias/materias/controllers/MateriasController.java
@@ -1,7 +1,15 @@
 package com.materias.materias.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.materias.materias.dto.MateriasEditDto;
+import com.materias.materias.dto.MateriasListDto;
+import com.materias.materias.entity.Materias;
+import com.materias.materias.service.MateriasService;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/materias")
 public class MateriasController {
 }
--- src/main/java/com/materias/materias/service/MateriasService.java
+++ src/main/java/com/materias/materias/service/MateriasService.java
@@ -1,8 +1,16 @@
 package com.materias.materias.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.materias.materias.entity.Materias;
+import com.materias.materias.repository.MateriasRepository;
+import com.materias.materias.dto.MateriasAddDto;
+import com.materias.materias.dto.MateriasEditDto;
+import com.materias.materias.dto.MateriasListDto;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MateriasService {
-    
+
 }
