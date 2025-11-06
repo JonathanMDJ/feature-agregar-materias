c8dcbda8f6e0418bf1c4728f84c3ba448549d24f
JonathanMDJ <2022371009@uteq.edu.mx>
2025-11-05 21:17:09 -0600

    Se creo la funcion de agregar, en las carpetas servicec y en el controlador

--- src/main/java/com/materias/materias/controllers/MateriasController.java
+++ src/main/java/com/materias/materias/controllers/MateriasController.java
@@ -3,13 +3,30 @@ package com.materias.materias.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.materias.materias.dto.MateriasEditDto;
-import com.materias.materias.dto.MateriasListDto;
+
+import com.materias.materias.dto.MateriasAddDto;
 import com.materias.materias.entity.Materias;
 import com.materias.materias.service.MateriasService;
-import java.util.List;
 
+/**
+ * Controlador REST para la gestión de materias.
+ * Historia de usuario: Agregar Materias - Jonathan Molina De Jesús
+ */
 @RestController
 @RequestMapping("/api/materias")
 public class MateriasController {
+
+    @Autowired
+    private MateriasService materiasService;
+
+    /**
+     * Endpoint para agregar una nueva materia al sistema
+     * param materiaDto DTO con los datos de la materia a crear
+     * return La materia creada
+     */
+    @PostMapping("/agregar-materia")
+    public ResponseEntity<Materias> agregarMateria(@RequestBody MateriasAddDto materiaDto) {
+        Materias newMateria = materiasService.agregarMateria(materiaDto);
+        return ResponseEntity.ok(newMateria);
+    }
 }
